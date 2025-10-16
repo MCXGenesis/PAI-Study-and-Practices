b5c777cb3b5e69656b43f40b160978d4941ef5b0
naufalmaula <naufal.maula@gmail.com>
2025-10-16 12:48:56 +0700

    giving useful comments

--- diffie_rsa.py
+++ diffie_rsa.py
@@ -16,21 +16,13 @@ def generate_shared_secret(A, b, p):
 
 n = p * q # rsa
 e = 3 # rsa, public key pair (e,n)
-flag = b'kuro'
+flag = SECRET
 flag_int = bytes_to_long(flag)
 A = generate_public_int(g,a,p_dh) # dh
 B = generate_public_int(g,b,p_dh) # dh
 shared_int = generate_shared_secret(A, b, p_dh) # dh, but mod too smol
 flag2 = flag_int ^ shared_int 
 c = pow(flag2, e, n) # rsa, ciphertext
-
-# # ---
-# t = (p-1)*(q-1)
-# # e smaller than t && e coprime with t
-# d = gmpy2.invert(e, t)
-# # d multiplicative inverse of  e mod t
-
-
 
 print(f"e = {e}")
 print(f"n = {n}")
@@ -40,13 +32,3 @@ print(f"g = {g}")
 print(f"A = {A}")
 print(f"B = {B}")
 print(f"t = {t}")
-
-# bxor = lambda a,b: bytes(x ^ y for x, y in zip(a, b))
-
-# b = B^g
-# shared_int = A^b
-# flag2 ,_ = gmpy2.iroot(c,e)
-# flag = flag2^shared_int
-# flag = long_to_bytes(flag)
-
-# print(flag.decode())
